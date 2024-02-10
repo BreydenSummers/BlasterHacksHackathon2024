92f05524a77022adf8c8e64fee5b1c01cccf5975
dylan <136660038+Throarin@users.noreply.github.com>
2024-02-10 10:48:12 -0700

    add some targeting logic into the update function of units class

--- units.py
+++ units.py
@@ -9,9 +9,21 @@ class Unit:
         self.color = color
         
 
-    def update(self):
+    def update(self, object_list):
         # You can implement any update logic here, such as movement or AI behavior
+        current_closest = Unit()
+        closest_distance = 10000
+        for object in object_list:
+            distance = ((self.x - object.x) ** 2 + (self.y - object.y) ** 2) ** 0.5
+            if object.team is not self.team and distance < closest_distance:
+                current_closest = object
+                closest_distance = distance
+
+            # TODO move the unit towards the enemies....ONWARDS!!!
         pass
 
     def render(self, screen, scroll_x, scroll_y):
         pygame.draw.rect(screen, self.color, (self.x + scroll_x, self.y + scroll_y, self.width, self.height))
+
+
+    
