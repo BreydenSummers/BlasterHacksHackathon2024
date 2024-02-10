54821e6ef46d34bb1b3194ada1f4f1a980202573
dylan <136660038+Throarin@users.noreply.github.com>
2024-02-09 23:27:21 -0700

    Update tower.py

--- tower.py
+++ tower.py
@@ -13,16 +13,22 @@ class tower :
         self.x = x
         self.y = y
 
-        # health bar attributes 
-        self.hbar_x = 0
-        self.hbar_y = 0
+        self.width = 150
+        self.height = 450
 
-    def draw_health(): 
-        pass
+        # health bar attributes 
+        self.hbar_x = (self.width/2)
+        self.hbar_y = y-self.height/8
+
+    def draw_health(self, screen, scroll_x, scroll_y): 
+        GREEN = (0, 255, 0)
+        pygame.draw.rect(screen, (0, 0, 0), (scroll_x + self.hbar_x-5, scroll_y + self.hbar_y-5, 60, 25))
+        pygame.draw.rect(screen, GREEN, (scroll_x + self.hbar_x, scroll_y + self.hbar_y, 50, 15))
 
     def render(self, screen, scroll_x, scroll_y):
         BLUE = (0, 0, 255)
-        pygame.draw.rect(screen, BLUE, (scroll_x + self.x, scroll_y + self.y, 150, 450))
+        self.draw_health(screen, scroll_x, scroll_y)
+        pygame.draw.rect(screen, BLUE, (scroll_x + self.x, scroll_y + self.y, self.width, self.height))
 
     def update(self, x_offset, y_offset):
 
